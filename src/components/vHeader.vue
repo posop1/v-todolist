87df215842534be2bff1320a428a73fc6bd43138
posop1 <artur.yakhin52@gmail.com>
2022-07-13 22:27:59 +0500

    init

---
+++ src/components/vHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <div class="container">
+        <h1>TodoList</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.header{
+  width: 100%;
+  background: rgba(0, 0, 0, 0.877);
+  padding: 15px 0;
+}
+  h1{
+    font-size: 32px;
+    color: white;
+  }
+</style>
